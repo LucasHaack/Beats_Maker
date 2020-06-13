e66d594eac626e76ad2cda3a1dd558b1dd06b5a3
Lucas Haack <lucashaack5@gmail.com>
2020-06-12 19:50:08 -0700

    Switch from switch cases to keyup/down codes

--- app.js
+++ app.js
@@ -156,226 +156,430 @@ purpleThree.addEventListener('mouseup', e => {
 });
 
 //Key Down
-window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
-    }
 
-    switch (event.key) {
+document.addEventListener('keydown', function(event) {
 
-//Green Buttons Key Down
+  //Purple Buttons Key Down
+  if (event.code == 'Numpad0') {
+    purpleOne.style.background = "rgb(206, 1, 206)";
+    purpleOne.style.scale="1.125";
+    purpleOne.style.transitionDuration=".15s";
+    console.log("0");
+  }
 
-      //case "Down": // IE/Edge specific value
-      case "7":
-        greenOne.style.background = "rgb(11, 255, 11)";
-        greenOne.style.scale="1.125";
-        greenOne.style.transitionDuration=".15s";
+  if (event.code == 'NumpadDecimal') {
+    purpleTwo.style.background = "rgb(206, 1, 206)";
+    purpleTwo.style.scale="1.125";
+    purpleTwo.style.transitionDuration=".15s";
+    console.log(".");
+  }
+//this probably won't work
+  if (event.key == 'Enter') {
+    purpleThree.style.background = "rgb(206, 1, 206)";
+    purpleThree.style.scale="1.125";
+    purpleThree.style.transitionDuration=".15s";
+    console.log("Enter");
+  }
+
+
+  //Red Buttons Key Down
+  if (event.code == 'Numpad1') {
+
+    redOne.style.background = "rgb(253, 8, 49)";
+    redOne.style.scale="1.125";
+    redOne.style.transitionDuration=".15s";
+    console.log("1");
+
+    audioOne.play();
+  }
+
+  if (event.code == 'Numpad2') {
+    redTwo.style.background = "rgb(253, 8, 49)";
+    redTwo.style.scale="1.125";
+    redTwo.style.transitionDuration=".15s";
+    console.log("2");
+
+    audioTwo.play();
+  }
+
+  if (event.code == 'Numpad3') {
+    redThree.style.background = "rgb(253, 8, 49)";
+    redThree.style.scale="1.125";
+    redThree.style.transitionDuration=".15s";
+    console.log("3");
+
+    audioThree.play();
+  }
+
+
+  //Blue Buttons Key Down
+  if (event.code == 'Numpad4') {
+    blueOne.style.background = "rgb(0, 162, 255)";
+    blueOne.style.scale="1.125";
+    blueOne.style.transitionDuration=".15s";
+    console.log("4");
+
+    audioFour.play();
+  }
+
+  if (event.code == 'Numpad5') {
+    blueTwo.style.background = "rgb(0, 162, 255)";
+    blueTwo.style.scale="1.125";
+    blueTwo.style.transitionDuration=".15s";
+    console.log("5");
+
+    audioFour.play();
+  }
+
+  if (event.code == 'Numpad6') {
+    blueThree.style.background = "rgb(0, 162, 255)";
+    blueThree.style.scale="1.125";
+    blueThree.style.transitionDuration=".15s";
+    console.log("6");
+  }
+
+  //Green Buttons Key Down
+  if (event.code == 'Numpad7') {
+    greenOne.style.background = "rgb(11, 255, 11)";
+    greenOne.style.scale="1.125";
+    greenOne.style.transitionDuration=".15s";
+    console.log("7");
+  }
+  
+  if (event.code == 'Numpad8') {
+    greenTwo.style.background = "rgb(11, 255, 11)";
+    greenTwo.style.scale="1.125";
+    greenTwo.style.transitionDuration=".15s";
+    console.log("8");
+  }
+  if (event.code == 'Numpad9') {
+    greenThree.style.background = "rgb(11, 255, 11)";
+    greenThree.style.scale="1.125";
+    greenThree.style.transitionDuration=".15s";
+    console.log("9");
+  }
+
+});
+
+//Key Up
+
+document.addEventListener('keyup', function(event) {
+
+  //Purple Buttons Key Up
+  if (event.code == 'Numpad0') {
+    purpleOne.style.background = "#640057";
+    purpleOne.style.scale="1"
+    console.log("Numpad0 up")
+  }
+
+  if (event.code == 'NumpadDecimal') {
+    purpleTwo.style.background = "#640057";
+    purpleTwo.style.scale="1"
+    console.log(". up")
+  }
+
+  if (event.key == "Enter") {
+    purpleThree.style.background = "#640057";
+    purpleThree.style.scale="1"
+    console.log("Enter up")
+  }
+
+
+  //Red Buttons Key Up
+  if (event.code == 'Numpad1') {
+
+    redOne.style.background = "#d30000";
+    redOne.style.scale="1"
+    console.log("Numpad1 up")
+
+    audioOne.pause();
+    audioOne.currentTime = 0;
+  }
+
+  if (event.code == 'Numpad2') {
+    redTwo.style.background = "#d30000";
+    redTwo.style.scale="1"
+    console.log("Numpad2 up")
+
+    audioTwo.pause();
+    audioTwo.currentTime = 0;
+  }
+
+  if (event.code == 'Numpad3') {
+    redThree.style.background = "#d30000";
+    redThree.style.scale="1"
+    console.log("Numpad3 up")
+
+    audioThree.pause();
+    audioThree.currentTime = 0;
+  }
+
+
+  //Blue Buttons Key Up
+  if (event.code == 'Numpad4') {
+    blueOne.style.background = "#003afa";
+    blueOne.style.scale="1"
+    console.log("Numpad4 up")
+
+    audioFour.pause();
+    audioFour.currentTime = 0;
+  }
+
+  if (event.code == 'Numpad5') {
+    blueTwo.style.background = "#003afa";
+    blueTwo.style.scale="1"
+    console.log("Numpad5 up")
+
+    audioFour.pause();
+    audioFour.currentTime = 0;
+  }
+
+  if (event.code == 'Numpad6') {
+    blueThree.style.background = "#003afa";
+    blueThree.style.scale="1"
+    console.log("Numpad6 up")
+  }
+
+  //Green Buttons Key Up
+  if (event.code == 'Numpad7') {
+    greenOne.style.background = "#25d302";
+    greenOne.style.scale="1"
+    console.log("Numpad7 up")
+  }
+  
+  if (event.code == 'Numpad8') {
+    greenTwo.style.background = "#25d302";
+    greenTwo.style.scale="1"
+    console.log("Numpad8 up")
+  }
+  if (event.code == 'Numpad9') {
+    greenThree.style.background = "#25d302";
+    greenThree.style.scale="1"
+    console.log("Numpad9 up")
+  }
+
+});
+
+// window.addEventListener("keydown", function (event) {
+//     if (event.defaultPrevented) {
+//       return; // Do nothing if the event was already processed
+//     }
+
+//     switch (event.key) {
+
+// //Green Buttons Key Down
+
+//       //case "Down": // IE/Edge specific value
+//       case "7":
+//         // greenOne.style.background = "rgb(11, 255, 11)";
+//         // greenOne.style.scale="1.125";
+//         // greenOne.style.transitionDuration=".15s";
         
-        break;
-      //case "Up": // IE/Edge specific value
-      case "8":
-        greenTwo.style.background = "rgb(11, 255, 11)";
-        greenTwo.style.scale="1.125";
-        greenTwo.style.transitionDuration=".15s";
-        this.console.log("8");
-        break;
+//         break;
+//       //case "Up": // IE/Edge specific value
+//       case "8":
+//         // greenTwo.style.background = "rgb(11, 255, 11)";
+//         // greenTwo.style.scale="1.125";
+//         // greenTwo.style.transitionDuration=".15s";
+//         // this.console.log("8");
+//         break;
 
-        //case "Up": // IE/Edge specific value
-      case "9":
-        greenThree.style.background = "rgb(11, 255, 11)";
-        greenThree.style.scale="1.125";
-        greenThree.style.transitionDuration=".15s";
-        this.console.log("9");
-        break;
+//         //case "Up": // IE/Edge specific value
+//       case "9":
+//         // greenThree.style.background = "rgb(11, 255, 11)";
+//         // greenThree.style.scale="1.125";
+//         // greenThree.style.transitionDuration=".15s";
+//         // this.console.log("9");
+//         break;
 
-//Blue Buttons Key Down
-      case "4":
-        blueOne.style.background = "rgb(0, 162, 255)";
-        blueOne.style.scale="1.125";
-        blueOne.style.transitionDuration=".15s";
-        this.console.log("4");
+// //Blue Buttons Key Down
+//       case "4":
+//         // blueOne.style.background = "rgb(0, 162, 255)";
+//         // blueOne.style.scale="1.125";
+//         // blueOne.style.transitionDuration=".15s";
+//         // this.console.log("4");
 
-        audioFour.play();
-        break;
+//         // audioFour.play();
+//         break;
 
-      case "5":
-        blueTwo.style.background = "rgb(0, 162, 255)";
-        blueTwo.style.scale="1.125";
-        blueTwo.style.transitionDuration=".15s";
-        this.console.log("5");
+//       case "5":
+//         // blueTwo.style.background = "rgb(0, 162, 255)";
+//         // blueTwo.style.scale="1.125";
+//         // blueTwo.style.transitionDuration=".15s";
+//         // this.console.log("5");
 
-        audioFour.play();
-        break;
+//         // audioFour.play();
+//         break;
 
-      case "6":
-        blueThree.style.background = "rgb(0, 162, 255)";
-        blueThree.style.scale="1.125";
-        blueThree.style.transitionDuration=".15s";
-        this.console.log("6");
-        break;
+//       case "6":
+//         // blueThree.style.background = "rgb(0, 162, 255)";
+//         // blueThree.style.scale="1.125";
+//         // blueThree.style.transitionDuration=".15s";
+//         // this.console.log("6");
+//         break;
 
-//Red Buttons Key Down
-      case "1":
-        redOne.style.background = "rgb(253, 8, 49)";
-        redOne.style.scale="1.125";
-        redOne.style.transitionDuration=".15s";
-        this.console.log("1");
+// //Red Buttons Key Down
+//       // case "1":
+//       //   redOne.style.background = "rgb(253, 8, 49)";
+//       //   redOne.style.scale="1.125";
+//       //   redOne.style.transitionDuration=".15s";
+//       //   this.console.log("1");
 
-        audioOne.play();
-        break;
+//       //   audioOne.play();
+//       //   break;
   
-      case "2":
-        redTwo.style.background = "rgb(253, 8, 49)";
-        redTwo.style.scale="1.125";
-        redTwo.style.transitionDuration=".15s";
-        this.console.log("2");
+//       case "2":
+//         // redTwo.style.background = "rgb(253, 8, 49)";
+//         // redTwo.style.scale="1.125";
+//         // redTwo.style.transitionDuration=".15s";
+//         // this.console.log("2");
 
-        audioTwo.play();
-        break;
+//         // audioTwo.play();
+//         break;
   
-      case "3":
-        redThree.style.background = "rgb(253, 8, 49)";
-        redThree.style.scale="1.125";
-        redThree.style.transitionDuration=".15s";
-        this.console.log("3");
+//       case "3":
+//         // redThree.style.background = "rgb(253, 8, 49)";
+//         // redThree.style.scale="1.125";
+//         // redThree.style.transitionDuration=".15s";
+//         // this.console.log("3");
 
-        audioThree.play();
-        break;    
+//         // audioThree.play();
+//         break;    
 
-//Purple Buttons Key Down
-      case "0":
-        purpleOne.style.background = "rgb(206, 1, 206)";
-        purpleOne.style.scale="1.125";
-        purpleOne.style.transitionDuration=".15s";
-        this.console.log("0");
-        break;
+// //Purple Buttons Key Down
+//       case "0":
+//         // purpleOne.style.background = "rgb(206, 1, 206)";
+//         // purpleOne.style.scale="1.125";
+//         // purpleOne.style.transitionDuration=".15s";
+//         // this.console.log("0");
+//         break;
 
-      case ".":
-        purpleTwo.style.background = "rgb(206, 1, 206)";
-        purpleTwo.style.scale="1.125";
-        purpleTwo.style.transitionDuration=".15s";
-        this.console.log(".");
-        break;
+//       case ".":
+//         // purpleTwo.style.background = "rgb(206, 1, 206)";
+//         // purpleTwo.style.scale="1.125";
+//         // purpleTwo.style.transitionDuration=".15s";
+//         // this.console.log(".");
+//         break;
 
-      case "Enter":
-        purpleThree.style.background = "rgb(206, 1, 206)";
-        purpleThree.style.scale="1.125";
-        purpleThree.style.transitionDuration=".15s";
-        this.console.log("Enter");
-        break;        
-}
+//       case "Enter":
+//         purpleThree.style.background = "rgb(206, 1, 206)";
+//         purpleThree.style.scale="1.125";
+//         purpleThree.style.transitionDuration=".15s";
+//         this.console.log("Enter");
+//         break;        
+// }
 
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-  }, true);
+//     // Cancel the default action to avoid it being handled twice
+//     event.preventDefault();
+//   }, true);
 
   
   //Key Up
-  window.addEventListener("keyup", function (event) {
-    if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
-    }
+  // window.addEventListener("keyup", function (event) {
+  //   if (event.defaultPrevented) {
+  //     return; // Do nothing if the event was already processed
+  //   }
 
-    switch (event.key) {
+  //   switch (event.key) {
 
 //Green Buttons Key Up
       //case "7": // IE/Edge specific value
-      case "7":
-        greenOne.style.background = "#25d302";
-        greenOne.style.scale="1"
-        this.console.log("Numpad7 up")
+      // case "7":
+      //   greenOne.style.background = "#25d302";
+      //   greenOne.style.scale="1"
+      //   this.console.log("Numpad7 up")
 
-        break;
+      //   break;
 
-
-      //case "7": // IE/Edge specific value
-      case "8":
-        greenTwo.style.background = "#25d302";
-        greenTwo.style.scale="1"
-        this.console.log("Numpad8 up")
-        break;
 
       //case "7": // IE/Edge specific value
-      case "9":
-        greenThree.style.background = "#25d302";
-        greenThree.style.scale="1"
-        this.console.log("Numpad9 up")
-        break;
+      // case "8":
+      //   greenTwo.style.background = "#25d302";
+      //   greenTwo.style.scale="1"
+      //   this.console.log("Numpad8 up")
+      //   break;
+
+      //case "7": // IE/Edge specific value
+      // case "9":
+      //   greenThree.style.background = "#25d302";
+      //   greenThree.style.scale="1"
+      //   this.console.log("Numpad9 up")
+      //   break;
 
 
 //Blue Buttons Key Up
-      case "4":
-        blueOne.style.background = "#003afa";
-        blueOne.style.scale="1"
-        this.console.log("Numpad4 up")
+      // case "4":
+      //   blueOne.style.background = "#003afa";
+      //   blueOne.style.scale="1"
+      //   this.console.log("Numpad4 up")
 
-        audioFour.pause();
-        audioFour.currentTime = 0;
-        break;
+      //   audioFour.pause();
+      //   audioFour.currentTime = 0;
+      //   break;
           
-      case "5":
-        blueTwo.style.background = "#003afa";
-        blueTwo.style.scale="1"
-        this.console.log("Numpad5 up")
+      // case "5":
+      //   blueTwo.style.background = "#003afa";
+      //   blueTwo.style.scale="1"
+      //   this.console.log("Numpad5 up")
 
-        audioFour.pause();
-        audioFour.currentTime = 0;
-        break;
+      //   audioFour.pause();
+      //   audioFour.currentTime = 0;
+      //   break;
 
-      case "6":
-        blueThree.style.background = "#003afa";
-        blueThree.style.scale="1"
-        this.console.log("Numpad6 up")
-        break;
+      // case "6":
+      //   blueThree.style.background = "#003afa";
+      //   blueThree.style.scale="1"
+      //   this.console.log("Numpad6 up")
+      //   break;
 
 //Red Buttons Key Up
-      case "1":
-        redOne.style.background = "#d30000";
-        redOne.style.scale="1"
-        this.console.log("Numpad1 up")
+      // case "1":
+      //   redOne.style.background = "#d30000";
+      //   redOne.style.scale="1"
+      //   this.console.log("Numpad1 up")
 
-        audioOne.pause();
-        audioOne.currentTime = 0;
-        break;
+      //   audioOne.pause();
+      //   audioOne.currentTime = 0;
+      //   break;
           
-      case "2":
-        redTwo.style.background = "#d30000";
-        redTwo.style.scale="1"
-        this.console.log("Numpad2 up")
+      // case "2":
+      //   redTwo.style.background = "#d30000";
+      //   redTwo.style.scale="1"
+      //   this.console.log("Numpad2 up")
 
-        audioTwo.pause();
-        audioTwo.currentTime = 0;
-        break;
+      //   audioTwo.pause();
+      //   audioTwo.currentTime = 0;
+      //   break;
 
-      case "3":
-        redThree.style.background = "#d30000";
-        redThree.style.scale="1"
-        this.console.log("Numpad3 up")
+      // case "3":
+      //   redThree.style.background = "#d30000";
+      //   redThree.style.scale="1"
+      //   this.console.log("Numpad3 up")
 
-        audioThree.pause();
-        audioThree.currentTime = 0;
-        break;
+      //   audioThree.pause();
+      //   audioThree.currentTime = 0;
+      //   break;
 
 //Purple Buttons Key Up
-      case "0":
-        purpleOne.style.background = "#640057";
-        purpleOne.style.scale="1"
-        this.console.log("Numpad0 up")
-        break;
+      // case "0":
+      //   purpleOne.style.background = "#640057";
+      //   purpleOne.style.scale="1"
+      //   this.console.log("Numpad0 up")
+      //   break;
           
-      case ".":
-        purpleTwo.style.background = "#640057";
-        purpleTwo.style.scale="1"
-        this.console.log(". up")
-        break;
+      // case ".":
+      //   purpleTwo.style.background = "#640057";
+      //   purpleTwo.style.scale="1"
+      //   this.console.log(". up")
+      //   break;
 
-      case "Enter":
-        purpleThree.style.background = "#640057";
-        purpleThree.style.scale="1"
-        this.console.log("Enter up")
-        break;
-}
+//       case "Enter":
+//         purpleThree.style.background = "#640057";
+//         purpleThree.style.scale="1"
+//         this.console.log("Enter up")
+//         break;
+// }
 
     // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-  }, true);
+  //   event.preventDefault();
+  // }, true);
